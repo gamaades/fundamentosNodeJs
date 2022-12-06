b8eca2b48cdac59bdffca11d4938e00c629d0ebc
gmanan <gamaades@gmail.com>
2022-12-05 23:32:37 -0300

    clase errores opk

--- modulos/errores.js
+++ modulos/errores.js
@@ -1,5 +1,31 @@
+function otraFuncion() {
+   seRompe();
+} 
+ 
  function seRompe() {
     return 3 + Z;
  }
 
- seRompe();
+ function seRompeAsincrona(cb) {
+   setTimeout(() => {
+      try {
+         return 3 + Z;
+      } catch (error) {
+         console.error("Error en mi función asincrona!");
+         cb(error);
+      }
+   }, 1000);
+ }
+
+ try {
+   // seRompe();
+   seRompeAsincrona( function (err) {
+      console.log(err.message);
+   });
+ } catch (error) {
+   console.error("Vaya, algo se a roto!!!");
+   console.error(error.message);
+   console.log("Pero no pasa nada, lo hemos capturado!");
+ }
+
+ console.log("Esto esta al final");
